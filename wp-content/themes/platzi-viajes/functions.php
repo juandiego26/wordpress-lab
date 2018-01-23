cbadd522f260294864440cda7d2a305701827494
Juan Diego <silgajuandiego@gmail.com>
2018-01-22 22:13:26 -0500

    [Add] Working with page builders and Custom Posts Type

--- wp-content/themes/platzi-viajes/functions.php
+++ wp-content/themes/platzi-viajes/functions.php
@@ -1,5 +1,5 @@
 <?php
-
+/*******ADD ROLE VIAJERO************/
 function add_role_viajero()
 {
   remove_role('viajero');
@@ -19,5 +19,46 @@ function add_role_viajero()
 
 // add the simple_role
 add_action('init', 'add_role_viajero');
+
+/*************ADD CUSTOM POST TYPE********************************/
+function viajes_init() {
+    $labels = array(
+        'name'              => _x( 'Viajes', 'post type general name', 'your-plugin-textdomain' ),
+        'singular_name'     => _x( 'Viajes', 'post type general name', 'your-plugin-textdomain' ),
+        'menu_name'         => _x( 'Mis viajes', 'admin menu', 'your-plugin-textdomain' ),
+        'name_admin_bar'    => _x( 'Viajes', 'add new on admin bar', 'your-plugin-textdomain' ),
+        'add_new'           => _x( 'Añadir nuevo', 'viaje', 'your-plugin-textdomain' ),
+        'add_new_item'      => __( 'Añadir nuevo viaje', 'your-plugin-textdomain' ),
+        'new_item'          => __( 'Nuevo viaje', 'your-plugin-textdomain' ),
+        'edit_item'         => __( 'Editar viaje', 'your-plugin-textdomain' ),
+        'view_item'         => __( 'Ver viaje', 'your-plugin-textdomain' ),
+        'all_items'         => __( 'Todos los viajes', 'your-plugin-textdomain' ),
+        'search_items'      => __( 'Buscar viajes', 'your-plugin-textdomain' ),
+        'parent_item_colon' => __( 'Viajes padre', 'your-plugin-textdomain' ),
+        'not_found'         => __( 'No hemos encontrado viajes.', 'your-plugin-textdomain' ),
+        'not_found_in_trash'=> __( 'No hemos encontrado viajes en la papelera', 'your-plugin-textdomain' ),
+    );
+
+    $args = array(
+        'labels'            => $labels,
+        'description'       => __('Description', 'your-plugin-textdomain'),
+        'public'            => true,
+        'public_queryable'  => true,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'viaje' ),
+        'capability_type'   => 'post',
+        'has_archive'       => true,
+        'hierarchical'      => false,
+        'menu_position'     => null,
+        'menu_icon'         => 'dashicons-admin-multisite',
+        'supports'          => array( 'title', 'editor', 'author', 'thumbnail' )
+    );
+
+    register_post_type( 'viaje', $args );
+}
+
+add_action( 'init', 'viajes_init' );
 
 ?>
