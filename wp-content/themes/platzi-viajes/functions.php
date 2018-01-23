c79295c64302aabc146000ae3f7efb8b021aa534
Juan Diego <silgajuandiego@gmail.com>
2018-01-23 17:17:11 -0500

    [Add] Content and design added into project

--- wp-content/themes/platzi-viajes/functions.php
+++ wp-content/themes/platzi-viajes/functions.php
@@ -60,7 +60,49 @@ function viajes_init() {
     register_post_type( 'viaje', $args );
 }
 
-add_action( 'init', 'viajes_init' );
+add_action( 'init', 'rutas_init' );
+
+/****************CPT RUTAS***********************************/
+function rutas_init() {
+    $labels = array(
+        'name'              => _x( 'Rutas', 'post type general name', 'your-plugin-textdomain' ),
+        'singular_name'     => _x( 'Rutas', 'post type general name', 'your-plugin-textdomain' ),
+        'menu_name'         => _x( 'Mis rutas', 'admin menu', 'your-plugin-textdomain' ),
+        'name_admin_bar'    => _x( 'Rutas', 'add new on admin bar', 'your-plugin-textdomain' ),
+        'add_new'           => _x( 'Añadir nueva', 'ruta', 'your-plugin-textdomain' ),
+        'add_new_item'      => __( 'Añadir nueva ruta', 'your-plugin-textdomain' ),
+        'new_item'          => __( 'Nueva ruta', 'your-plugin-textdomain' ),
+        'edit_item'         => __( 'Editar ruta', 'your-plugin-textdomain' ),
+        'view_item'         => __( 'Ver ruta', 'your-plugin-textdomain' ),
+        'all_items'         => __( 'Todas las rutas', 'your-plugin-textdomain' ),
+        'search_items'      => __( 'Buscar rutas', 'your-plugin-textdomain' ),
+        'parent_item_colon' => __( 'Rutas padre', 'your-plugin-textdomain' ),
+        'not_found'         => __( 'No hemos encontrado rutas.', 'your-plugin-textdomain' ),
+        'not_found_in_trash'=> __( 'No hemos encontrado rutas en la papelera', 'your-plugin-textdomain' ),
+    );
+
+    $args = array(
+        'labels'            => $labels,
+        'description'       => __('Description', 'your-plugin-textdomain'),
+        'public'            => true,
+        'public_queryable'  => true,
+        'show_ui'           => true,
+        'show_in_rest'      => true,
+        'show_in_menu'      => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'ruta' ),
+        'capability_type'   => 'post',
+        'has_archive'       => true,
+        'hierarchical'      => false,
+        'menu_position'     => null,
+        'menu_icon'         => 'dashicons-admin-multisite',
+        'supports'          => array( 'title', 'editor', 'author', 'thumbnail' )
+    );
+
+    register_post_type( 'ruta', $args );
+}
+
+add_action( 'init', 'rutas_init' );
 
 /********Advanced custom post type fields***********/
 
@@ -153,6 +195,62 @@ if(function_exists("register_field_group"))
 					'param' => 'post_type',
 					'operator' => '==',
 					'value' => 'viaje',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
+
+/******************Advanced Custom Post type RUTAS**************************/
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_ruta',
+		'title' => 'Ruta',
+		'fields' => array (
+			array (
+				'key' => 'ruta_dificultad',
+				'label' => 'Dificultad',
+				'name' => 'dificultad',
+				'type' => 'select',
+				'choices' => array (
+					'baja' => 'Baja',
+					'media' => 'Media',
+					'alta' => 'Alta',
+					'experto' => 'Experto',
+				),
+				'default_value' => '',
+				'allow_null' => 0,
+				'multiple' => 0,
+			),
+			array (
+				'key' => 'ruta_tiempo',
+				'label' => 'Tiempo',
+				'name' => 'tiempo',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'ruta',
 					'order_no' => 0,
 					'group_no' => 0,
 				),
